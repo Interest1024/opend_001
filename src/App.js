901136fdd5c9da18c23587880a398c568fc961c0
Interest1024 <leo.liubin@gmail.com>
2019-09-13 23:25:18 +1000

    Small adjustment

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
+import Report from './components/Report';
 import Contact from './components/Contact';
 import About from './components/About';
 import Post from './components/Post';
@@ -14,6 +14,7 @@ function App() {
       <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="/report" component={Report} />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
             <Route path="/posts/:post_id" component={Post} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { Link, NavLink, withRouter } from 'react-router-dom';
+import { NavLink, withRouter } from 'react-router-dom';
 
 const Navbar = (props) => {
-  /*
-  setTimeout(() => {
-    props.history.push('/about')
-  }, 2000);
-  */
   return (
-    <nav className="nav-wrapper red darken-3">
+    <nav className="nav-wrapper blue darken-3">
       <div className="container">
-        <a className="brand-logo" href=" ">Poke'Times</a>
+        <a className="brand-logo" href=" ">Opend</a>
         <ul className="right">
-          <li><Link to="/">Home</Link></li>
+          <li><NavLink to="/">Home</NavLink></li>
+          <li><NavLink to="/report">Report</NavLink></li>
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/contact">Contact</NavLink></li>
         </ul>
